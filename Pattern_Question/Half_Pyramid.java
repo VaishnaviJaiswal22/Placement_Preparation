36e01dce6a7bc36941a038c012a1e90b6bb4262e
Vaishnavi Jaiswal <144608967+VaishnaviJaiswal22@users.noreply.github.com>
2024-12-25 15:03:00 +0530

    Add files via upload

---
+++ Pattern_Question/Half_Pyramid.java
@@ -0,0 +1,14 @@
+package apna_college_Pattern_Question;
+
+public class Half_Pyramid {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<4;i++) {
+			for(int j = 0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
