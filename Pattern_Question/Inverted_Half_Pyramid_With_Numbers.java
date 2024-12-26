b2dd4dc0040788f3a50da3d65f6ccc93549969bd
Vaishnavi Jaiswal <144608967+VaishnaviJaiswal22@users.noreply.github.com>
2024-12-26 12:31:17 +0530

    Add files via upload

---
+++ Pattern_Question/Inverted_Half_Pyramid_With_Numbers.java
@@ -0,0 +1,15 @@
+package apna_college_Pattern_Question;
+
+public class Inverted_Half_Pyramid_With_Numbers {
+
+	public static void main(String[] args) {
+		for(int i = 5;i>0;i--) {
+			for(int j = 1;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
