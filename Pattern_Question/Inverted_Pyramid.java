780ec662bc1e2b7cfd35dbd997527f1b8c3d8215
Vaishnavi Jaiswal <144608967+VaishnaviJaiswal22@users.noreply.github.com>
2024-12-29 12:49:38 +0530

    Add files via upload

---
+++ Pattern_Question/Inverted_Pyramid.java
@@ -0,0 +1,15 @@
+package apna_college_Pattern_Question;
+
+public class Inverted_Pyramid {
+
+	public static void main(String[] args) {
+		for(int i = 4;i>=0;i--) {
+			for(int j = 1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
